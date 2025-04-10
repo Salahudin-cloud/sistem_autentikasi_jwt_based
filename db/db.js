41d8e8cfcc3fcd73a90037a7e19e1c904ae4e229
Salahudin-cloud <ayubi54321@gmail.com>
2025-04-10 18:11:19 +0700

    fixed changes  and protect sensitive data

--- db/db.js
+++ db/db.js
@@ -1,11 +1,11 @@
+require("dotenv").config()
 const mysql = require("mysql2")
 
-
 const db = mysql.createConnection({
-    host : 'localhost',
-    user : "RadsDev",
-    password : "belajar_sql123",
-    database : "belajar_sql"
+    host : process.env.HOST_DB,
+    user : process.env.USER_DB,
+    password : process.env.PASSWORD_DB,
+    database : process.env.USER_DB
 })  
 
 db.connect(err => {
